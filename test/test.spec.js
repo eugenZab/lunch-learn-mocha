d6dc43771ab937fad57991bfe6d1f7741c2d4ff9
EugeneZ <eugenez@invenco.com>
2019-11-27 10:33:59 +1300

    nit commit

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,11 +1,26 @@
 const expect = require('chai').expect;
 
-describe('Test Suite 1', function(){
-    it('Test 1 - Pass', function(){
-        expect('test').to.have.lengthOf(4);
-    })
+const multiplyByFive = function(value, done){
+    setTimeout(function(){
+        if(value === 10){
+            done (value * 4)
+        }
+        done (value * 5);
+    }, 1000)
+}
 
-    it('Test 2 - Fail', function(){
-        expect('testt').to.have.lengthOf(4);
+
+describe('Test Suite 1', function(){
+    it('Test 1 - Pass', function(done){
+        multiplyByFive(5,function(result){
+            expect(result).to.be.equal(25, "5 * 5 results is incorect");
+            done();
+        })
+    })
+    it('Test 2 - Fail', function(done){
+        multiplyByFive(10,function(result){
+            expect(result).to.be.equal(50, "10 * 5 results is incorect");
+            done();
+        })
     })
 })
